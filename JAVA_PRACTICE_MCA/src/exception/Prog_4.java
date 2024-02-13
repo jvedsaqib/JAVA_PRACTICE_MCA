7b82c5658b6c905ecfdcc2379f9164b039261b8a
Saqib Javed <jvedsaqib6291@gmail.com>
2024-02-13 14:43:30 +0530

    DSA SEM - II MCA (Array Problems)

--- JAVA_PRACTICE_MCA/src/exception/Prog_4.java
+++ JAVA_PRACTICE_MCA/src/exception/Prog_4.java
@@ -4,47 +4,48 @@ package exception;
 		throws an exception if it is passed a non-alphabetic character.
  */
 
-
 class NotALetterException extends Exception {
 
-	  static int count=0;
-	  String msg;
-	  
-	  
-	  public NotALetterException(String msg) {
-	    this.msg = msg;
-	  }
-	  
+	static int count = 0;
+	String msg;
 
-	  public static void countLetter(char c) throws NotALetterException {
-	  
+	public NotALetterException(String msg) {
+		this.msg = msg;
+	}
 
-	    if (Character.isAlphabetic(c)) {
-	      count++;
-	    }
-	    else {
-	      throw new NotALetterException(c + " is not a letter"); 
-	    }
-	  
-	  
-	  }
+	public static void countLetter(char c) throws NotALetterException {
+
+		if (Character.isAlphabetic(c)) {
+			count++;
+		} else {
+			throw new NotALetterException(c + " is not a letter");
+		}
 
 	}
 
+	public int getCount() {
+		return count;
+	}
+
+}
+
 public class Prog_4 {
 
-	 public static void main(String[] args) {
-			String s = "abcj2sad";
-			char arr[] = s.toCharArray();
-			
-			try {
-				for(char c : arr) {
-					NotALetterException.countLetter(c);
-				}
-				System.out.println(NotALetterException.count);
-			}catch(NotALetterException e) {
-				System.out.println(e.msg);
+	public static void main(String[] args) {
+		String s = "abcj2sad";
+		char arr[] = s.toCharArray();
+
+		try {
+			for (char c : arr) {
+				NotALetterException.countLetter(c);
 			}
+			System.out.println(NotALetterException.count);
+		} catch (NotALetterException e) {
+			System.out.println(e.msg);
 		}
+		finally {
+			System.out.println(NotALetterException.count);
+		}
+	}
 
 }
