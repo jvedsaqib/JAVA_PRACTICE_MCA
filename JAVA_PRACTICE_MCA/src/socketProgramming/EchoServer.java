cc3746b797db71897fbcf48e7ca36d196d9a8ba9
Saqib Javed <99673719+jvedsaqib@users.noreply.github.com>
2024-05-10 07:55:16 +0530

    socket

---
+++ JAVA_PRACTICE_MCA/src/socketProgramming/EchoServer.java
@@ -0,0 +1,56 @@
+package socketProgramming;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class EchoServer 
+{
+	public static void main(String args[]) throws Exception
+    	{
+	
+        		ServerSocket sersock = null;
+        		String line;
+			Scanner is=null;   
+        		PrintStream os=null;
+        		Socket clisock = null;        		     
+	
+	        	try 
+			{
+           			sersock = new ServerSocket(1342);
+        		}
+        		catch (IOException e) 
+			{
+           			System.out.println(e);
+       	 		}   
+
+			//(Optional)To confirm Server Reserved specified port or not
+        		System.out.println("The Server has reserved port No.: "+sersock.getLocalPort()+" for this Service");  
+
+	    		try 
+			{
+           			clisock = sersock.accept();
+			
+				//To confirm Server communicated through the socket or not        
+        			System.out.println("Client with IP Address "+clisock.getInetAddress()+" has communicated via port No.: "+clisock.getPort());  
+ 
+           			is = new Scanner(clisock.getInputStream());   
+           			os = new PrintStream(clisock.getOutputStream());
+
+	   			// As long as we receive data, echo that data back to the client
+           			while (true) 
+	   			{
+             				line = is.nextLine();
+             				os.println(line); 
+           			}
+        		}   
+    			catch (Exception e) 
+			{
+           			System.out.println(e);
+        		}
+			sersock.close();
+			clisock.close();
+     			is.close();
+     			os.close();
+    	}
+}
